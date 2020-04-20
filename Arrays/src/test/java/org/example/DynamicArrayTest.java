9ea98982638d13fa969a14a6fa63d40da7755a71
al <apizano@purdue.edu>
2020-04-19 23:56:24 -0500

    adding test for Insert

--- Arrays/src/test/java/org/example/DynamicArrayTest.java
+++ Arrays/src/test/java/org/example/DynamicArrayTest.java
@@ -1,13 +1,14 @@
 package org.example;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class DynamicArrayTest {
     DynamicArray<String> array;
 
-    @BeforeClass
+    @Before
     public void setUp() {
         array = new DynamicArray(10);
     }
@@ -24,6 +25,15 @@ public class DynamicArrayTest {
 
     @Test
     public void insertTest() {
+        array.add("1");
+        array.add("4");
+        array.add("8");
+        
+        array.insert(1,"4");
+
+        Assert.assertEquals("1", array.get(0));
+        Assert.assertEquals("4", array.get(1));
+        Assert.assertEquals("8", array.get(2));
 
     }
 
