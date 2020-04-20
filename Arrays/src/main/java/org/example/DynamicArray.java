b5b69bc0d1df078106c897b7a5c5763e064440da
al <apizano@purdue.edu>
2020-04-19 20:47:21 -0500

    adding methods for Dynamic array and Unit Tests

--- Arrays/src/main/java/org/example/DynamicArray.java
+++ Arrays/src/main/java/org/example/DynamicArray.java
@@ -1,12 +1,29 @@
 package org.example;
 
-public class DynamicArray {
-    String[] array;
+public class DynamicArray<String> {
+    Object[] array;
     int size;
     int capacity;
-    
-    public DynamicArray(int capacity) {
-        array = new String[capacity];
 
+    public DynamicArray() {
+        array = new Object[0];
+        size = 0;
+        capacity =0;
+    }
+    public DynamicArray(int capacity) {
+        array = new Object[capacity];
+        size = 0;
+    }
+
+    public String get(int index){
+        return (String)array[index];
+    }
+
+    public void insert(int index, String val) {
+        // Check size
+
+        // Copy elements
+
+        // Insert elements
     }
 }
---
+++ Arrays/src/test/java/org/example/DynamicArrayTest.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class DynamicArrayTest {
+    @BeforeClass
+    public void setUp() {
+        DynamicArray<String> array = new DynamicArray();
+    }
+
+    @Test
+    public void getTest() {
+
+    }
+
+    @Test
+    public void setTest() {
+
+    }
+
+    @Test
+    public void insertTest() {
+        
+    }
+}
