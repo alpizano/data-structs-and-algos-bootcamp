66955f12a0969edbfb54b105d565aad2bd6cb20b
al <apizano@purdue.edu>
2020-04-19 20:22:07 -0500

    Implementing dynamic array class, wrapper class around Array

---
+++ Arrays/src/main/java/org/example/DynamicArray.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class DynamicArray {
+    String[] array;
+    int size;
+    int capacity;
+    
+    public DynamicArray(int capacity) {
+        array = new String[capacity];
+
+    }
+}
