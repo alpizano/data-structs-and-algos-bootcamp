312b13afa383be63dba0f84d87015dca4afa32d4
al <apizano@purdue.edu>
2020-04-20 21:31:31 -0500

    Adding sample tests

--- LinkedList/src/test/java/org/example/LinkedListTest.java
+++ LinkedList/src/test/java/org/example/LinkedListTest.java
@@ -1,4 +1,18 @@
 package org.example;
 
+import org.junit.Before;
+import org.junit.Test;
+
 public class LinkedListTest {
+    LinkedList list;
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void Test() {
+        
+    }
 }
