29ab8bfae96f5057ed8901a9f54992ec72dbacd0
al <apizano@purdue.edu>
2020-04-21 23:52:55 -0500

    Adding Queue class and unit tests for Queue

--- StacksAndQueues/src/main/java/org/example/Queue.java
+++ StacksAndQueues/src/main/java/org/example/Queue.java
@@ -1,4 +1,16 @@
 package org.example;
 
 public class Queue {
+    private static class Node {
+        int val;
+        Node next;
+
+        public Node(int val) {
+            this.val = val;
+        }
+    }
+
+    private Node head;
+
+    
 }
