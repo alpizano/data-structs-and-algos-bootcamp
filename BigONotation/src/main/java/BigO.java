5264863b90a16b849ca2e042e705ccd0979442d9
al <apizano@purdue.edu>
2020-04-21 21:11:41 -0500

    Adding BigO notation class with common big o runtime complexities

---
+++ BigONotation/src/main/java/BigO.java
@@ -0,0 +1,67 @@
+public class BigO {
+
+    /**
+     * O(n) + O(n) -> O(2n) = O(n)
+     * @param array
+     */
+    public void foo(int[] array) {
+
+        for (int i = 0; i < array.length; i++) {
+            // ...
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            // ...
+        }
+
+    }
+
+    /**
+     * O(n*m)
+     * @param array1
+     * @param array2
+     */
+    public void bar(int[] array1, int[] array2) {
+
+        for (int i = 0; i < array1.length; i++) {
+            // ...
+            for (int j = 0; j < array2.length; j++) {
+                // ...
+            }
+        }
+
+    }
+
+    /**
+     * O(n^2)
+     * @param array
+     */
+    public void baz(int[] array) {
+
+        for (int i = 0; i < array.length; i++) {
+            // ...
+            for (int j = 0; j < array.length; j++) {
+                // ...
+                if (array[i] < array[j]) {
+                    // Launch!
+                }
+            }
+        }
+
+    }
+
+    public void beep(int[] array) {
+
+        for (int i = 0; i < array.length; i++) {
+            // ...
+            for (int j = 0; j < array.length; j++) {
+                // ...
+                for (int k = 0; k < 9999999; k++) {
+                    // Jump!
+                }
+            }
+        }
+
+    }
+
+}
