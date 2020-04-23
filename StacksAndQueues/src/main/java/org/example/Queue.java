2bc1aa4b22969056d731f206551c5f30bec78e2b
al <apizano@purdue.edu>
2020-04-22 20:19:53 -0500

    added test to print queue and remove method

--- StacksAndQueues/src/main/java/org/example/Queue.java
+++ StacksAndQueues/src/main/java/org/example/Queue.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.EmptyStackException;
+
 public class Queue {
     private static class Node {
         int val;
@@ -15,16 +17,51 @@ public class Queue {
     private Node tail;
 
     public void add(int val) {
+        // Create New Node
         Node newNode = new Node(val);
 
-        if(head == null) {
-            head = newNode;
-            tail = newNode;
-            return;
+        // Special case when Queue is empty
+        if(tail != null) {
+            // Point Tail's reference to the newly created Node
+            tail.next = newNode;
         }
-        
 
+        // Update tail reference; increment up by 1 so it points to last element in queue
+        tail = newNode;
 
-
+        // Special case when Queue is empty
+        if(head == null) {
+            head = tail;
+        }
     }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public int remove() {
+        if(head == null) {
+            throw new EmptyStackException();
+        }
+
+        int val = head.val;
+        head = head.next;
+        return val;
+    }
+
+    public void print() {
+        for(Node cursor = head; cursor != null; cursor=cursor.next) {
+            System.out.print(cursor.val + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue queue = new Queue();
+        queue.add(1);
+        queue.add(5);
+        queue.add(10);
+
+        queue.print();
+    }
+
 }
--- StacksAndQueues/src/test/java/org/example/QueueTest.java
+++ StacksAndQueues/src/test/java/org/example/QueueTest.java
@@ -1,5 +1,18 @@
 package org.example;
 
-public class QueueTest {
+import org.junit.Before;
+import org.junit.Test;
 
+public class QueueTest {
+    Queue queue;
+
+    @Before
+    public void setUp() {
+        queue = new Queue();
+    }
+
+    @Test
+    public void Test() {
+
+    }
 }
