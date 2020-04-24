583ad7a26d8b161d87dfa5a1bbe51ff167637d34
al <apizano@purdue.edu>
2020-04-24 01:03:56 -0500

    Writing test for put()

---
+++ HashTables/src/test/java/org/example/HashTableTest.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class HashTableTest {
+    HashTable table;
+
+    @Before
+    public void setup() {
+        table = new HashTable();
+    }
+
+    @Test
+    public void put() {}
+    
+}
