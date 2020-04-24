51db3c6e7607872465d71b772087f61863249824
al <apizano@purdue.edu>
2020-04-23 21:36:49 -0500

    finished hash function algorithm which uses javas built in hash function

--- HashTables/src/main/java/org/example/HashTable.java
+++ HashTables/src/main/java/org/example/HashTable.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
 import java.util.Arrays;
 
 public class HashTable {
@@ -24,7 +26,24 @@ public class HashTable {
 
     public void put(String key, String val) {
         // Get index
+        int index = getIndex(key);
 
+    }
+
+    // Get returns Value for Key parameter
+    public String get(String key) {
+
+    }
+
+    // Hashing algorithm logic inside getIndex
+    private int getIndex(String key) {
+        int hashCode = key.hashCode();
+        System.out.println("hashCode=");
+
+        // Convert to index. Bit wise AND to force positive index
+        int index = hashCode & 0x7fffffff % CAPACITY;
+
+        System.out.println("index = " + index);
     }
 
     @Override
