50fdb8a8b3659310e7cf5e9f2a18499e6d38ecab
al <apizano@purdue.edu>
2020-05-17 21:25:40 -0500

    adding constructor for node nested class

--- BinaryTrees/src/main/java/org/example/BinarySearchTree.java
+++ BinaryTrees/src/main/java/org/example/BinarySearchTree.java
@@ -1,5 +1,16 @@
 package org.example;
 
 public class BinarySearchTree {
+    static class Node {
+        int val;
+        Node left, right;
+
+        public Node(int val) {
+            this.val = val;
+
+        }
+    }
+
+    Node root;
 
 }
