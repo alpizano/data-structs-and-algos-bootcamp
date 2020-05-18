fea6732b93e277ccb252a79879fbb98f8772b9f8
al <apizano@purdue.edu>
2020-05-17 21:36:34 -0500

    Implementing find algorithm for binary search tree

--- BinaryTrees/src/main/java/org/example/BinarySearchTree.java
+++ BinaryTrees/src/main/java/org/example/BinarySearchTree.java
@@ -2,15 +2,31 @@ package org.example;
 
 public class BinarySearchTree {
     static class Node {
-        int val;
+        int key;
+        String val;
         Node left, right;
 
-        public Node(int val) {
+        public Node(int key, String val) {
+            this.key = key;
             this.val = val;
-
         }
     }
 
     Node root;
 
+
+    private Node find(Node node, int key) {
+        if(node == null || node.key == key) {
+            return node;
+        }
+        else if(node.key < key) {
+            return find(node.left, key);
+        }
+        else if(node.key > key) {
+            return find(node.right, key);
+        }
+
+        // If key is not found in BST, return null
+        return null;
+    }
 }
